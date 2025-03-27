f01858a0fcebed5037d1cbef5b9d7d94c586b0b4
warren-ajio <warren@ajio.co.jp>
2025-03-27 18:13:20 +0800

    Initial Commit
    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,259 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import React, {useEffect, useState} from 'react';
+import {
+  Button,
+  CircularProgress,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {createUsers, deleteUser, getUsers, updateUser} from "@/app/api/user";
+
+interface IUser {
+  id: bigint;
+  created_at: Date;
+  updated_at: Date;
+  name: string;
+}
+
+export default function UserTable() {
+  const [open, setOpen] = useState<boolean>(false)
+
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState<string>('')
+  const [dialogType, setDialogType] = useState<string>("")
+  const [users, setUsers] = useState<IUser[]>([])
+  const [selectedUser, setSelectedUser] = useState<IUser>()
+
+  const handleGetUser = async () => {
+    try {
+      setLoading(true)
+      const res = await getUsers()
+
+      if (res) {
+        setUsers(res)
+        setLoading(false)
+      }
+    } catch (error) {
+      console.log("error - getUser", error)
+    }
+  }
+
+  const handleCreateUser = async () => {
+    try {
+      setLoading(true)
+      const res = await createUsers({name})
+
+      if (res) {
+        handleCloseDialog()
+        setLoading(false)
+        await handleGetUser()
+      }
+    } catch (error) {
+      console.log("error - createUser", error)
+    }
+  }
+
+  const handleUpdateUser = async () => {
+    try {
+      setLoading(true)
+      if (selectedUser) {
+        const res = await updateUser(selectedUser.id, selectedUser)
+
+        if (res) {
+          handleCloseDialog()
+          setLoading(false)
+          await handleGetUser()
+        }
+      }
+    } catch (error) {
+      console.log("error - updateUser", error)
+    }
+  }
+
+  const handleDeleteUser = async () => {
+    try {
+      setLoading(true)
+      if (selectedUser) {
+        const res = await deleteUser(selectedUser.id)
+
+        if (res) {
+          handleCloseDialog()
+          setLoading(false)
+          await handleGetUser()
+        }
+      }
+    } catch (error) {
+      console.log("error - deleteUser", error)
+    }
+  }
+
+  const handleCloseDialog = () => {
+    setOpen(false)
+    setName("")
+  }
+
+  const handleSetName = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleSetSelectedUser = (e) => {
+    setSelectedUser(prev => {
+      return {
+        ...prev ?? {},
+        name: e.target.value
+      }
+    })
+  }
+  const handleOpenDialog = (type: string, user?: IUser) => () => {
+    if (user) {
+      setSelectedUser((user))
+    }
+    setDialogType(type)
+    setOpen(true)
+  }
+
+  useEffect(() => {
+    handleGetUser()
+  }, [])
+
+  const displayDialog = () => {
+    switch (dialogType) {
+      case "add":
+      case "edit":
+        return (
+            <Dialog open={open} onClose={handleCloseDialog}>
+              <DialogTitle>{dialogType === "add" ? "Create" : "Update"} User</DialogTitle>
+              <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Name"
+                    fullWidth
+                    variant="outlined"
+                    value={dialogType === "add" ? name : selectedUser?.name}
+                    onChange={dialogType === "add" ? handleSetName : handleSetSelectedUser}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleCloseDialog} color="error">
+                  Cancel
+                </Button>
+                <Button
+                    onClick={dialogType === "add" ? handleCreateUser : handleUpdateUser}
+                    color="primary"
+                >
+                  {dialogType === "add" ? "Create" : "Update"}
+                </Button>
+              </DialogActions>
+            </Dialog>
+        )
+      case "delete":
+        return (
+            <Dialog open={open} onClose={handleCloseDialog}>
+              <DialogTitle>Confirm Delete</DialogTitle>
+              <DialogContent>
+                <Typography>
+                  Are you sure you want to delete{" "}
+                  <strong>{selectedUser?.name}</strong>? This action cannot be undone.
+                </Typography>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleCloseDialog} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleDeleteUser} color="error">
+                  Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
+        )
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      <Container maxWidth="md" sx={{mt: 4}}>
+        <Typography variant="h5" gutterBottom>
+          User Management
+        </Typography>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* CREATE BUTTON */}
+        <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon/>}
+            sx={{mb: 2, float: "right"}}
+            onClick={handleOpenDialog("add")}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Create User
+        </Button>
+
+        {/* TABLE */}
+        <TableContainer component={Paper} sx={{overflowX: "auto"}}>
+          <Table>
+            <TableHead>
+              <TableRow sx={{backgroundColor: "#1976d2"}}>
+                <TableCell sx={{color: "white"}}>ID</TableCell>
+                <TableCell sx={{color: "white"}}>Name</TableCell>
+                <TableCell sx={{color: "white"}}>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {loading ? (
+                  <TableRow>
+                    <TableCell colSpan={4} align="center">
+                      <CircularProgress/>
+                    </TableCell>
+                  </TableRow>
+              ) : users.length > 0 ? (
+                  users.map((user) => (
+                      <TableRow key={user.id}>
+                        <TableCell>{user.id}</TableCell>
+                        <TableCell>{user.name}</TableCell>
+                        <TableCell>
+                          <IconButton
+                              color="primary"
+                              onClick={handleOpenDialog("edit", user)}
+                          >
+                            <EditIcon/>
+                          </IconButton>
+                          <IconButton
+                              color="error"
+                              onClick={handleOpenDialog("delete", user)}
+                          >
+                            <DeleteIcon/>
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                  ))
+              ) : (
+                  <TableRow>
+                    <TableCell colSpan={4} align="center">
+                      No users found
+                    </TableCell>
+                  </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        {displayDialog()}
+      </Container>
   );
 }
